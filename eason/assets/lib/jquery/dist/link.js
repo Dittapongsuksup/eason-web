ecc72a7411ca2c764d50b42f232bfeffe8c01b2a
veerapols <veerapol.71export@gmail.com>
2023-05-17 15:13:43 +0700

    update link web

--- eason/assets/lib/jquery/dist/link.js
+++ eason/assets/lib/jquery/dist/link.js
@@ -2,3 +2,60 @@
 function myFunction() {
   window.open("https://weblink.set.or.th/dat/news/202305/0861NWS120520231717320366T.pdf");
 }
+function myFunction1() {
+  window.open("https://weblink.set.or.th/dat/news/202305/0861NWS120520231717320366T.pdf");
+}
+function myFunction2() {
+  window.open("https://weblink.set.or.th/dat/news/202305/0861NWS120520231717320366T.pdf");
+}
+function myFunction3() {
+  window.open("https://weblink.set.or.th/dat/news/202305/0861NWS120520231717320366T.pdf");
+}
+function myFunction4() {
+  window.open("https://weblink.set.or.th/dat/news/202305/0861NWS120520231717320366T.pdf");
+}
+function myFunction5() {
+  window.open("https://weblink.set.or.th/dat/news/202305/0861NWS120520231717320366T.pdf");
+}
+function myFunction6() {
+  window.open("https://weblink.set.or.th/dat/news/202305/0861NWS120520231717320366T.pdf");
+}
+function myFunction7() {
+  window.open("https://weblink.set.or.th/dat/news/202305/0861NWS120520231717320366T.pdf");
+}
+function myFunction8() {
+  window.open("https://weblink.set.or.th/dat/news/202305/0861NWS120520231717320366T.pdf");
+}
+function myFunction9() {
+  window.open("https://weblink.set.or.th/dat/news/202305/0861NWS120520231717320366T.pdf");
+}
+function myFunction10() {
+  window.open("https://weblink.set.or.th/dat/news/202305/0861NWS120520231717320366T.pdf");
+}
+function myFunction11() {
+  window.open("https://weblink.set.or.th/dat/news/202305/0861NWS120520231717320366T.pdf");
+}
+function myFunction12() {
+  window.open("https://weblink.set.or.th/dat/news/202305/0861NWS120520231717320366T.pdf");
+}
+function myFunction13() {
+  window.open("https://weblink.set.or.th/dat/news/202305/0861NWS120520231717320366T.pdf");
+}
+function myFunction14() {
+  window.open("https://weblink.set.or.th/dat/news/202305/0861NWS120520231717320366T.pdf");
+}
+function myFunction15() {
+  window.open("https://weblink.set.or.th/dat/news/202305/0861NWS120520231717320366T.pdf");
+}
+function myFunction16() {
+  window.open("https://weblink.set.or.th/dat/news/202305/0861NWS120520231717320366T.pdf");
+}
+function myFunction17() {
+  window.open("https://weblink.set.or.th/dat/news/202305/0861NWS120520231717320366T.pdf");
+}
+function myFunction18() {
+  window.open("https://weblink.set.or.th/dat/news/202305/0861NWS120520231717320366T.pdf");
+}
+function myFunction19() {
+  window.open("https://weblink.set.or.th/dat/news/202305/0861NWS120520231717320366T.pdf");
+}
